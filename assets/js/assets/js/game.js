e2823126402d4a57bcfc7549165724768b564884
Nick Garza <thenickgarza14@gmail.com>
2021-11-16 09:13:14 -0600

    initial/game

--- assets/js/assets/js/game.js
+++ assets/js/assets/js/game.js
@@ -1,1 +1,5 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    var playerName = window.prompt("What is your robot's name?");
+    console.log(playerName);
+}
+fight();
